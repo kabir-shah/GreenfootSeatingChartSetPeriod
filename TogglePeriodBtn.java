a1238268250cafb0059075d6ed6b9e2c135f4af2
Oleby0406 <oleg.bychenkov@gmail.com>
2020-09-14 17:45:25 -0700

    add period switch button

---
+++ TogglePeriodBtn.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TogglePeriodBtn here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TogglePeriodBtn extends Actor
+{
+    private int currentPeriod;
+    public TogglePeriodBtn() {
+        currentPeriod = 3;
+        setImage("images/period3.jpg");
+    }
+    public int getCurrentPeriod() {
+    	return currentPeriod;
+    }
+    public void act() {
+        if (Greenfoot.mousePressed(this)) {
+        	if (currentPeriod == 3) {
+                	currentPeriod = 2;
+                	setImage("images/period2.jpg");
+        	}
+        	else {
+        		currentPeriod = 3;
+        		setImage("images/period3.jpg");
+        	}
+        	//getWorld().setPeriod(currentPeriod);
+        }
+    }
+
+}
